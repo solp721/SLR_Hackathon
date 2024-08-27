3687383bc0e85d09f6ae93504530126275764371
solp721 <easter721@gmail.com>
2024-08-27 16:11:30 +0900

    feat : 욕설 감지 ai api 추가

---
+++ 2024_korea_sw_hackathon/api/ai/reject/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const instance = axios.create({
+	baseURL: process.env.NEXT_PUBLIC_SLR_AI_CHECK_API_URL,
+	headers: {
+		'Content-Type': 'application/json',
+	},
+});
+
+// 욕설감지 post
+export const checkReview = async checkValue => {
+	try {
+		const response = await instance.post('/filter', checkValue);
+		return response;
+	} catch (error) {
+		throw new Error(
+			error.response?.data?.message || '리뷰 제출이 거부되었습니다.',
+		);
+	}
+};
